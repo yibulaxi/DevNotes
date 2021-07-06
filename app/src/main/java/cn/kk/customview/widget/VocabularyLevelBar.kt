181ea7852d3f8687be07491cb372390b9bf03bc1
zhangkx <kamaihamaiha@163.com>
2021-07-06 12:47:32 +0800

    处理按下、抬起 圆环应该放置的位置，临时用小红点代替。

--- app/src/main/java/cn/kk/customview/widget/VocabularyLevelBar.kt
+++ app/src/main/java/cn/kk/customview/widget/VocabularyLevelBar.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 import cn.kk.customview.R
@@ -13,13 +14,19 @@ import cn.kk.customview.utils.ValueUtil
  * 单词等级拖动条
  * 1. 绘制背景 ok
  * 2. 绘制单词等级锚点 ok
- * 3. 绘制选中区域背景
+ * 3. 绘制选中区域背景 ok
  * 4. 绘制选中区域端点. 默认是一星（第一个）ok
  * 5. 支持拖拽
  * 6. 支持放手后修正
+ * 7. 支持按下后更新区间，抬起手后修正区间
  */
+
+// 单词等级默认 5个 级别
+val DEFAULT_VOCABULARY_LEVEL_COUNT = 5
+
 class VocabularyLevelBar(mContext: Context,val attributeSet: AttributeSet?): View(mContext,attributeSet) {
     val TAG = this::class.java.simpleName
+
 
     // 未选中背景颜色
     var normalBgColor: Int = ResourcesCompat.getColor(mContext.resources,R.color.grey_c,null)
@@ -52,22 +59,35 @@ class VocabularyLevelBar(mContext: Context,val attributeSet: AttributeSet?): Vie
         style = Paint.Style.STROKE
     }
 
+    val paintPressTemp = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.RED
+    }
+
 
     // 数字参数
     // 1. 单词等级锚点左右间距
     val selectedPointMargin = ValueUtil.dp2px(30f)
     // 2. 单词等级数量
-    val levelCount = 5
+    val levelCount = DEFAULT_VOCABULARY_LEVEL_COUNT
     // 3. 单词等级锚点，半径
     val levelAnchorRadius = ValueUtil.dp2px(5f)
-    // 4. 单词等级区间，头和尾圆环，半径.
+    // 4. 单词等级区间，选中圆环，半径（绘制时的半径）.
     val selectedLevelRingRadius by lazy {
         levelAnchorRadius + (paintAnchorRingSelected.strokeWidth / 2)
     }
-    val startLevel = 0
+    // 5.  单词等级区间，选中圆环，半径（视觉上真实的半径）
+    val selectedLevelVersionRealRadius by lazy {
+        levelAnchorRadius + paintAnchorRingSelected.strokeWidth
+    }
+
+    // 选中的最低等级. 默认是 1星
+    var startLevel = 0
+
+    // 选中的最高等级. 默认是 1星
+    var endLevel = 0
 
     // 锚点数组
-    val anchorsArray = arrayOfNulls<PointF>(5)
+    val anchorsArray = arrayOfNulls<PointF>(levelCount)
 
     init {
 
@@ -109,19 +129,170 @@ class VocabularyLevelBar(mContext: Context,val attributeSet: AttributeSet?): Vie
 
 
         // 3. 绘制选中区域背景
-        val selectAreaPath = Path().apply {
-            moveTo(anchorsArray[1]!!.x, 0f)
-            lineTo(anchorsArray[3]!!.x, 0f)
-            lineTo(anchorsArray[3]!!.x,height.toFloat())
-            lineTo(anchorsArray[1]!!.x, height.toFloat())
-            close()
+
+        correctLevelRegion()
+
+        if (startLevel != endLevel) {
+            canvas.drawPath(Path().apply {
+                moveTo(anchorsArray[startLevel]!!.x, 0f)
+                lineTo(anchorsArray[endLevel]!!.x, 0f)
+                lineTo(anchorsArray[endLevel]!!.x, height.toFloat())
+                lineTo(anchorsArray[startLevel]!!.x, height.toFloat())
+                close()
+            }, paintSelectedBg)
         }
-        canvas.drawPath(selectAreaPath,paintSelectedBg)
 
         // 4. 绘制选中锚点区间的端点
-        drawSelectAnchor(1,canvas)
-        drawSelectAnchor(3,canvas)
+        drawSelectAnchor(startLevel, canvas)
+        if (startLevel != endLevel) {
+            drawSelectAnchor(endLevel, canvas)
+        }
 
+        // 5. 绘制临时实心球
+        if (pressEvent){
+            canvas.drawCircle(currentPressP.x, currentPressP.y,10f,paintPressTemp)
+        } else {
+            if (closestAnchorIndex != -1) {
+                val anchorCenterPWhenUP = getAnchorCenterP(closestAnchorIndex)
+                canvas.drawCircle(anchorCenterPWhenUP.x, anchorCenterPWhenUP.y, 10f, paintPressTemp)
+            }
+        }
+    }
+
+    /**
+     * 修正单词等级范围
+     */
+    private fun correctLevelRegion() {
+        endLevel = Math.max(0, endLevel)
+        endLevel = Math.min(endLevel, levelCount - 1)
+        startLevel = Math.max(0, startLevel)
+        startLevel = Math.min(startLevel, endLevel)
+    }
+
+    /**
+     * 获取锚点圆心
+     * @param index 如果 index 范围超过正常范围，则返回第一个锚点圆心
+     */
+    private fun getAnchorCenterP(index: Int): PointF{
+        if (index < 0 || index >= anchorsArray.size) return anchorsArray[0]!!
+        return anchorsArray[index]!!
+    }
+
+    /**
+     * 按下时，是否可以拖拽选中的锚点
+     */
+    private fun canDragWhenTouch(selectedLevel: Int, touchX: Float, touchY: Float): Boolean{
+        var index = selectedLevel
+        if (selectedLevel != startLevel && selectedLevel != endLevel){
+            index = startLevel
+        }
+        val startAnchorCenterP = getAnchorCenterP(index)
+        val diffX = Math.abs(touchX - startAnchorCenterP.x)
+        val diffY = Math.abs(touchY - startAnchorCenterP.y)
+        val distanceSquare = diffX * diffX + diffY * diffY
+        if (distanceSquare < selectedLevelVersionRealRadius * selectedLevelVersionRealRadius){
+            return true
+        }
+        return false
+    }
+
+    // 是否能腿拽
+    var canDraft = false
+    var lastTouchX = -1f
+    var pressEvent = false
+
+    // 当前手机按下的点
+    var currentPressP = PointF()
+
+    // 手指抬起后，最近的锚点索引
+    var closestAnchorIndex = -1
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+
+        // 5. 处理拖拽
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                // 记录按下屏幕的区域
+                printLog("按压屏幕：${event.x}, ${event.x}")
+
+                // 判断按下区域，是否在选中锚点区域。
+                // 如果按下的点 距离锚点圆心小于，锚点圆环的半径，那么就在选中区域里面, => 就可以拖拽
+                val startAnchorCenterP = getAnchorCenterP(startLevel)
+                val diffX = Math.abs(event.x - startAnchorCenterP.x)
+                val diffY = Math.abs(event.y - startAnchorCenterP.y)
+                val distanceSquare = diffX * diffX + diffY * diffY
+                if (distanceSquare < selectedLevelVersionRealRadius * selectedLevelVersionRealRadius){
+                    canDraft = true
+                }
+
+                canDraft = canDragWhenTouch(startLevel, event.x, event.y)
+
+                if (!canDraft && startLevel != endLevel){
+                    // 如果上面的不能拖拽，再判断高等级的锚点圆心距离
+                    canDraft = canDragWhenTouch(endLevel, event.x, event.y)
+                }
+
+                printLog("可拖拽：$canDraft")
+                if (canDraft){
+                    lastTouchX = event.x
+                }
+
+                // 处理按下后，选中圆环的最新位置（临时，手指离开屏幕后需要规正）
+                pressEvent = true
+                currentPressP.apply {
+                    x = event.x
+                    y = height / 2f
+                }
+                invalidate()
+            }
+            MotionEvent.ACTION_UP -> {
+                canDraft = false
+                pressEvent = false
+                // todo 离开屏幕，寻找最近的
+                printLog("离开屏幕：${event.x}, ${event.x}")
+
+                closestAnchorIndex = findClosestAnchor(event.x)
+                invalidate()
+            }
+            MotionEvent.ACTION_MOVE -> {
+                printLog("拖动：${event.x}, ${event.x}")
+                if (!canDraft){
+                    return false
+                }
+                // 计算水平移动距离，屏蔽掉 y 轴的拖动
+                val moveDistanceX = event!!.x - lastTouchX
+                printLog("水平移动：${moveDistanceX}")
+                // 绘制临时圆环
+                // 获取触摸点最近的锚点圆环圆心坐标
+                val closestRingPoint = getAnchorCenterP(startLevel)
+                val tempRingCenterP = PointF().apply {
+                    x = closestRingPoint.x + moveDistanceX
+                    y = closestRingPoint.y
+                }
+//                drawSelectAnchor(startLevel,)
+            }
+
+        }
+
+        lastTouchX = event!!.x
+        return true
+    }
+
+    /**
+     * 寻找最近的锚点
+     */
+    private fun findClosestAnchor(x: Float): Int {
+        var targetAnchorIndex = 0
+        var minDistance = Math.abs(x - anchorsArray[0]!!.x)
+        for (index in 1 until anchorsArray.size){
+            val tempDistance = Math.abs(x - anchorsArray[index]!!.x)
+            if (minDistance > tempDistance){
+                minDistance = tempDistance
+                targetAnchorIndex = index
+            }
+        }
+        return targetAnchorIndex
     }
 
     /**
@@ -139,31 +310,12 @@ class VocabularyLevelBar(mContext: Context,val attributeSet: AttributeSet?): Vie
     }
 
 
-    /*override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        setMeasuredDimension(measureMyWidth(widthMeasureSpec),measureMyHeight(heightMeasureSpec))
+    /**
+     * 打印日志
+     */
+    fun printLog(info: String){
+        Log.d(TAG, "printLog: $info")
     }
 
-   private fun measureMyWidth(measureSpec: Int): Int{
-        val specMode = MeasureSpec.getMode(measureSpec)
-        val specWidth = MeasureSpec.getSize(measureSpec)
-
-        return when(specMode){
-            MeasureSpec.UNSPECIFIED -> specWidth
-            MeasureSpec.AT_MOST -> measuredWidth
-            MeasureSpec.EXACTLY -> specWidth
-            else -> 0
-        }
-    }
-    private fun measureMyHeight(measureSpec: Int): Int{
-        val specMode = MeasureSpec.getMode(measureSpec)
-        val specWidth = MeasureSpec.getSize(measureSpec)
-
-        return when(specMode){
-            MeasureSpec.UNSPECIFIED -> specWidth
-            MeasureSpec.AT_MOST -> measuredWidth
-            MeasureSpec.EXACTLY -> specWidth
-            else -> 0
-        }
-    }*/
 
 }
