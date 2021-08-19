627abd0c7003bfd879e7a4ab2fe6accdd4a3eddc
kk <kamaihamaiha@163.com>
2021-08-19 16:50:42 +0800

    增加日期工具方法

---
+++ base/src/main/java/cn/kk/base/utils/DataHelper.java
@@ -0,0 +1,37 @@
+package cn.kk.base.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DataHelper {
+
+    /**
+     * 根据地区和语言，获取本地化的日期。
+     * @param language
+     * @param date
+     * @return
+     */
+    public static String localizeMonthDay(String language, Date date) {
+        SimpleDateFormat format;
+        if ("es".equals(language)) {//d m
+            format = new SimpleDateFormat("d MMM", new Locale("Es", "es"));
+        } else if ("fr".equals(language)) {//d m.
+            format = new SimpleDateFormat("d MMM", Locale.FRENCH);
+        } else if ("de".equals(language)) {//d. m
+            format = new SimpleDateFormat("d. MMM", Locale.GERMAN);
+        } else {//d m
+            format = new SimpleDateFormat("d MMM", Locale.ENGLISH);
+        }
+        return format.format(date);
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("en: " + localizeMonthDay("en", new Date()));
+        System.out.println("fr: " + localizeMonthDay("fr", new Date()));
+        System.out.println("de: " + localizeMonthDay("de", new Date()));
+        System.out.println("es: " + localizeMonthDay("es", new Date()));
+    }
+}
