f8eae68bf56949cce0de26702fbba54a6c148490
kk <kamaihamaiha@gmail.com>
2021-10-27 07:37:14 +0800

    播放动画，沿 Y轴

--- app/src/main/java/cn/kk/customview/ImmersiveModeActivity.kt
+++ app/src/main/java/cn/kk/customview/ImmersiveModeActivity.kt
@@ -1,10 +1,18 @@
 package cn.kk.customview
 
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.view.WindowInsetsController
+import android.view.animation.AnimationSet
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.addListener
+import androidx.core.animation.doOnEnd
+import kotlinx.android.synthetic.main.activity_immersive_mode.*
 
 /**
  * 沉浸式模式
@@ -19,5 +27,33 @@ class ImmersiveModeActivity: AppCompatActivity() {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         // 设置系统状态栏颜色为透明
         window.statusBarColor = Color.TRANSPARENT
+
+
+        val anim1 = ObjectAnimator.ofFloat(iv_avatar, "rotationY", -10f, 10f)
+        val anim2 = ObjectAnimator.ofFloat(iv_avatar, "rotationY", 10f, -10f)
+
+        val animSet = AnimatorSet().apply {
+            playSequentially(anim1, anim2)
+            duration = 1500
+        }
+
+        animSet.addListener(object: Animator.AnimatorListener{
+            override fun onAnimationStart(animation: Animator?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                animSet.start()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+
+        })
+        animSet.start()
+
     }
 }
