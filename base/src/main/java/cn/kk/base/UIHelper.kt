15e3de1588813961915b3d5f683e553105a996b0
kk <kamaihamaiha@163.com>
2021-10-28 19:05:08 +0800

    新增颜色操作方法

--- base/src/main/java/cn/kk/base/UIHelper.kt
+++ base/src/main/java/cn/kk/base/UIHelper.kt
@@ -33,4 +33,30 @@ object UIHelper {
         }
         return R.style.Theme_Default
     }
+
+
+    /**
+     * 把颜色转换成 hex 字符串形式
+     * @param color
+     * @return
+     */
+    fun color2HexEncoding(color: Int): String? {
+        var R: String
+        var G: String
+        var B: String
+        val sb = StringBuffer()
+        R = Integer.toHexString(Color.red(color))
+        G = Integer.toHexString(Color.green(color))
+        B = Integer.toHexString(Color.blue(color))
+        //判断获取到的R,G,B值的长度 如果长度等于1 给R,G,B值的前边添0
+        R = if (R.length == 1) "0$R" else R
+        G = if (G.length == 1) "0$G" else G
+        B = if (B.length == 1) "0$B" else B
+        sb.append("0x")
+        sb.append(R)
+        sb.append(G)
+        sb.append(B)
+        return sb.toString()
+    }
+
 }
