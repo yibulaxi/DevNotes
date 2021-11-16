c38994c268f3a7dd6f5a32cdae1696bfdec36a0c
kk <kamaihamaiha@gmail.com>
2021-11-17 07:37:52 +0800

    update

--- app/src/main/java/cn/kk/customview/ui/system/TextViewWidthEnableChangeActivity.kt
+++ app/src/main/java/cn/kk/customview/ui/system/TextViewWidthEnableChangeActivity.kt
@@ -6,17 +6,24 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_textview_enable_change.*
 
 /**
- * view 高度：0.5dp、1dp、2dp
+ * 可变的 TextView 最后要限制 maxWidth 
  */
 class TextViewWidthEnableChangeActivity: AppCompatActivity() {
-
+    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_textview_enable_change)
 
 
         btn_more.setOnClickListener {
-            tv_info.append(".")
+
+            if (count % 2 == 0){
+                tv_info.append("_")
+            } else {
+                tv_info.append(".")
+            }
+
+            count++
         }
     }
 }
