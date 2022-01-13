bafc2d40345b0fcddaa1081271b5f48a32337c5e
kk <kamaihamaiha@gmail.com>
2022-01-13 23:09:19 +0800

    update 022 自定义 selector 以透明前景切换控件

--- app/src/main/java/cn/kk/customview/ui/cool300/chapter1/Chapter1.kt
+++ app/src/main/java/cn/kk/customview/ui/cool300/chapter1/Chapter1.kt
@@ -20,6 +20,7 @@ class Chapter1: BaseActivity() {
             add("005. 自定义 CheckBox 风格")
             add("009. Drawable shape 作为 btn 背景")
             add("010. Drawable shape 渐变圆角按钮")
+            add("022. 自定义 selector 以透明前景切换控件")
         }
     }
 
@@ -30,6 +31,7 @@ class Chapter1: BaseActivity() {
                 0 -> openNextUI(Simple_005::class.java, itemList[position])
                 1 -> openNextUI(Simple_009::class.java, itemList[position])
                 2 -> openNextUI(Simple_010::class.java, itemList[position])
+                3 -> openNextUI(Simple_022::class.java, itemList[position])
             }
         }
     }
---
+++ app/src/main/java/cn/kk/customview/ui/cool300/chapter1/Simple_022.kt
@@ -0,0 +1,20 @@
+package cn.kk.customview.ui.cool300.chapter1
+
+import cn.kk.base.UIHelper
+import cn.kk.base.activity.BaseActivity
+import cn.kk.customview.R
+import kotlinx.android.synthetic.main.simple_009.*
+
+/**
+ * 自定义 selector 以透明前景切换控件
+ */
+class Simple_022: BaseActivity() {
+    override fun getLayout(): Int {
+        return R.layout.simple_022
+    }
+
+    override fun doWhenOnCreate() {
+        super.doWhenOnCreate()
+
+    }
+}
