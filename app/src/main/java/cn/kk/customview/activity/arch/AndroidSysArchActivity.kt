ea7d6ca9ad3bdb35233020feef250537c0314dc1
kk <kamaihamaiha@gmail.com>
2022-04-17 21:12:53 +0800

    mvc 简介

---
+++ app/src/main/java/cn/kk/customview/activity/arch/AndroidSysArchActivity.kt
@@ -0,0 +1,11 @@
+package cn.kk.customview.activity.arch
+
+import cn.kk.base.activity.BaseActivity
+import cn.kk.customview.R
+
+class AndroidSysArchActivity: BaseActivity() {
+    override fun getLayout(): Int {
+        return R.layout.activity_android_sys_arch
+    }
+
+}
--- app/src/main/java/cn/kk/customview/fragment/ArchFragment.kt
+++ app/src/main/java/cn/kk/customview/fragment/ArchFragment.kt
@@ -6,10 +6,7 @@ import androidx.cardview.widget.CardView
 import cn.kk.base.activity.BaseActivity
 import cn.kk.base.fragment.BaseFragment
 import cn.kk.customview.R
-import cn.kk.customview.activity.arch.JetPackActivity
-import cn.kk.customview.activity.arch.MvcActivity
-import cn.kk.customview.activity.arch.MvpActivity
-import cn.kk.customview.activity.arch.MvvmActivity
+import cn.kk.customview.activity.arch.*
 import java.util.jar.JarEntry
 
 class ArchFragment: BaseFragment() {
@@ -23,6 +20,7 @@ class ArchFragment: BaseFragment() {
         rootView.findViewById<CardView>(R.id.card_mvp).setOnClickListener { openMVP() }
         rootView.findViewById<CardView>(R.id.card_mvvm).setOnClickListener { openMVVM() }
         rootView.findViewById<CardView>(R.id.card_jetpack).setOnClickListener { openJetPack() }
+        rootView.findViewById<CardView>(R.id.card_android_sys).setOnClickListener { openAndroidSysArch() }
     }
 
    private fun openMVC(){
@@ -40,6 +38,11 @@ class ArchFragment: BaseFragment() {
    private fun openJetPack(){
        getBaseActivity()?.openNextUI(JetPackActivity::class.java, "JetPack")
     }
+
+    private fun openAndroidSysArch() {
+       getBaseActivity()?.openNextUI(AndroidSysArchActivity::class.java, "Android 体系架构")
+    }
+
 
     private fun getBaseActivity(): BaseActivity ?{
         if (activity is BaseActivity) return (activity as BaseActivity)
