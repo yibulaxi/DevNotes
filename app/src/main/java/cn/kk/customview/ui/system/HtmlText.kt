86f0312abd49f7b61874493ac5f6b607152265b1
kk <kamaihamaiha@gmail.com>
2022-05-26 23:39:52 +0800

    html

--- app/src/main/java/cn/kk/customview/ui/system/HtmlText.kt
+++ app/src/main/java/cn/kk/customview/ui/system/HtmlText.kt
@@ -1,6 +1,7 @@
 package cn.kk.customview.ui.system
 
 import android.text.Html
+import android.widget.TextView
 import cn.kk.base.activity.BaseActivity
 import cn.kk.customview.R
 import kotlinx.android.synthetic.main.activity_html_text.*
@@ -18,5 +19,9 @@ class HtmlText: BaseActivity() {
 
         tv_html.setText(Html.fromHtml(getString(R.string.html_demo)))
         tv_html_ting.setText(Html.fromHtml(getString(R.string.html_ting)))
+
+//        tv_recite.text = Html.fromHtml(String.format(getString(R.string.recite_learning_progress_new_word), 3, 9, 2))
+
+        tv_recite.text = String.format("%d%s", 13, "%")
     }
 }
