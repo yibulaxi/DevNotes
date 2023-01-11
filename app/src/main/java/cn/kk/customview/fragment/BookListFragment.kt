a50506677cad54e3e85db008b4088aece71d4aea
kk <kamaihamaiha@163.com>
2023-01-11 13:48:01 +0800

    book item 点击效果调整

--- app/src/main/java/cn/kk/customview/fragment/BookListFragment.kt
+++ app/src/main/java/cn/kk/customview/fragment/BookListFragment.kt
@@ -2,6 +2,7 @@ package cn.kk.customview.fragment
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
@@ -41,13 +42,17 @@ class BookListFragment: BaseFragment() {
             layoutManager = GridLayoutManager(context, 3)
             adapter = object : BaseQuickAdapter<BookModel, BaseViewHolder>(R.layout.item_book_view, bookListV2){
                 override fun convert(holder: BaseViewHolder, item: BookModel) {
-                    if (item.bookImgRes == -1) {
-                        holder.setText(R.id.tv_name, item.title)
+                    val tv = holder.getView<TextView>(R.id.tv_name)
+                    if (item.bookImgRes == -1) { // background 主题色透明度，按压效果水波纹（颜色为主题色）
+                        tv.text = item.title
                         holder.getView<CardView>(R.id.root_view).setCardBackgroundColor(ContextCompat.getColor(context, R.color.primary_10))
                         holder.setBackgroundResource(R.id.tv_name, 0)
-                    } else {
+                        tv.setBackgroundResource(R.drawable.bg_press_ripple_book)
+                        holder.itemView.foreground = null
+
+                    } else { // background 为书封面，按压效果封面上加上黑色遮罩
                         holder.setText(R.id.tv_name, "")
-                        holder.setBackgroundResource(R.id.tv_name, item.bookImgRes)
+                        tv.setBackgroundResource(item.bookImgRes)
                         holder.itemView.foreground = ContextCompat.getDrawable(context, R.drawable.bg_press_dark_selector)
                     }
 
